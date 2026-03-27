0b963601c5ea723fa892aad6fb8ee4a5b698b9ba
Ahmed Nasser Mohammed <enjoyplayax@gmail.com>
2026-03-27 05:09:19 +0200

    cleanup

--- MoldV1/mold-astro/src/preprocessing/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Preprocessing Module Index
- */
-export { TextCleaner } from './TextCleaner';
-export { Chunker } from './Chunker';
-export { KeywordExtractor } from './KeywordExtractor';
--- MoldV1/mold-astro/src/storage/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Storage Module Index
- */
-export { FilePersistence } from './PersistenceLayer';
-export type { PersistenceLayer, StorageBackend } from './PersistenceLayer';
